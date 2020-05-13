3676f66dc0e3f3524e722bb67c4234520f3c00be
hunseok329 <kha5064@gmail.com>
2020-05-13 22:27:02 +0900

    #Crawling part

--- main.py
+++ main.py
@@ -12,4 +12,23 @@ def get_last_pages():
     for link in links:
         pages.append(link.string)
     return max(pages)
-print(get_last_pages())
+
+def get_problem_word(html):
+    tda = html.find("td", {"style":"padding-left:5px;"})
+    # number = html.find_all("div", {"style":"text-align:center;"})
+    return tda
+
+def get_problems(last):
+    for page in range(last):
+        result = requests.get(f"{URL}?pg={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        table = soup.find("table", {"class":"grid"})
+        for word in table:
+            problem = get_problem_word(word)
+            print(problem)
+
+def get_jobs():
+    last_page = get_last_pages()
+    get_problems(int(last_page))
+
+get_jobs()
