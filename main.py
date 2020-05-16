0faa6d24426967d09d2a2b31d2a42b1617bf1b97
hunseok329 <kha5064@gmail.com>
2020-05-16 16:51:35 +0900

    #make crawling part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+# 프로젝트 오일러 사이트 한국사이터에서 원본 사이트로 변경 중
 
 URL = 'http://euler.synap.co.kr/prob_list.php'
 
@@ -19,11 +20,13 @@ def get_last_pages():
 
 def get_problem_word(html):
     tda = html.find("td", {"style": "padding-left:5px;"})
-    fir = html.find("div", {"style": "text-align:center;"})
+    fir = html.find("div")
     try:
         anchor = tda.find("a").text
         number = fir.find("b").string
-        return {"achor": anchor, "number": number}
+        parent = tda.next_sibling.next_sibling.find("div")
+        human = parent.get_text(strip=True).strip("\t").strip("\n")
+        return {"achor": anchor, "number": number, "human": human}
     except AttributeError:
         return None
 
@@ -39,6 +42,7 @@ def get_problems(last):
         # 문제 타이틀 부분이 같이 크롤링 되어 get_problem_word 부분에서 크롤링이 실패함 clear
         # a 태그를 가져오는 부분에서 None 타입이 있는 부분이 있는데 그부분에서 string 값으로 변환 실패 clear
         # a 태그 안에 다른 태그 들이 포함되어 있는 a 태그가 있어 string 변화에서 NoneTpye 값이 나오게 됨 clear
+        # div 태드 안에 있는 푼 사람 숫자를 가져와야함
         for word in tr:
             problem = get_problem_word(word)
             if problem is not None:
